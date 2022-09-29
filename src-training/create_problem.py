ad9b54d0b3e4d8e4e2f303cbd6bccaeb467290ba
isirmt - Ito Miri <52734487+isirmt@users.noreply.github.com>
2022-09-29 15:45:59 +0900

    multiprocessing

--- src-training/create_problem.py
+++ src-training/create_problem.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+from multiprocessing import Pool, cpu_count
 from sklearn import preprocessing
 import numpy as np
 import random
@@ -9,7 +9,7 @@ nb_voice = 88
 max_len = 185000
 dif_nom = np.arange(0,10000,100)
 problem_len = 10000
-nb_training = 50000
+nb_training = 1000
 voice_num = 0
 
 data_x = np.empty(0)
@@ -41,59 +41,59 @@ data_arr = np.reshape(data_arr,[nb_voice, max_len])
 print(data_arr)
 print(len_arr)
 
-for i in range(nb_training):
+with open('./data/data_x.csv', 'a') as f_x:
+    with open('./data/data_y.csv', 'a') as f_y:
+        for j in range(50):
+            for i in range(nb_training):
 
-    mix = random.randint(3,19)
-    x = np.zeros(problem_len)
-    y = np.zeros(nb_voice)
+                mix = random.randint(3,19)
+                x = np.zeros(problem_len)
+                y = np.zeros(nb_voice)
 
-    num = 0
-    y[voice_num] = random.randint(0,1)
+                num = 0
+                y[voice_num] = random.randint(0,1)
 
-    while num < mix:
-        ran_num = random.randint(0,nb_voice - 1)
-        if y[ran_num] == 0:
-            y[ran_num] = 1
-            num += 1
+                while num < mix:
+                    ran_num = random.randint(0,nb_voice - 1)
+                    if y[ran_num] == 0:
+                        y[ran_num] = 1
+                        num += 1
 
-    voice_max = 0
-    for p in range(nb_voice):
-        if len_arr[p] > voice_max and y[ran_num] == 1:
-            voice_max = len_arr[p]
-    
-    ans = [0,1]
-    if y[voice_num] == 1:
-        ans = [1,0]
+                voice_max = 0
+                for p in range(nb_voice):
+                    if len_arr[p] > voice_max and y[ran_num] == 1:
+                        voice_max = len_arr[p]
+                
+                ans = [0,1]
+                if y[voice_num] == 1:
+                    ans = [1,0]
 
-    if ans[0] == 1:
-        voice_max = len_arr[voice_num]
+                if ans[0] == 1:
+                    voice_max = len_arr[voice_num]
 
-    dif_arr = np.empty(0)
-    dif_max = 0
-    for p in range(nb_voice):
-        if y[p] == 1:
-            dif = random.choice(dif_nom)
-            dif_arr = np.append(dif_arr,dif)
-            if(dif_max < dif):
-                dif_max = dif
+                dif_arr = np.empty(0)
+                dif_max = 0
+                for p in range(nb_voice):
+                    if y[p] == 1:
+                        dif = random.choice(dif_nom)
+                        dif_arr = np.append(dif_arr,dif)
+                        if(dif_max < dif):
+                            dif_max = dif
 
-    loc = random.randint(0,int(voice_max - (problem_len + dif_max)))
-    num = 0
-    for p in range(nb_voice):
-        if y[p] == 1:
-            x += data_arr[p][int(dif_arr[num] + loc):int(dif_arr[num] + loc + problem_len)]
-            num += 1
+                loc = random.randint(0,int(voice_max - (problem_len + dif_max)))
+                num = 0
+                for p in range(nb_voice):
+                    if y[p] == 1:
+                        x += data_arr[p][int(dif_arr[num] + loc):int(dif_arr[num] + loc + problem_len)]
+                        num += 1
 
-    x = np.abs(np.fft.fft(x))
-    x = preprocessing.scale(x)
-    data_x = np.append(data_x,x)
-    data_y = np.append(data_y,ans)
+                x = np.abs(np.fft.fft(x))
+                x = preprocessing.scale(x)
+                data_x = np.append(data_x,x)
+                data_y = np.append(data_y,ans)
 
-    if i % 100 == 0:
-        print(f"training : {i}")
+                if i % 100 == 0:
+                    print(f"training : {i}")
 
-data_x = np.reshape(data_x,[nb_training,problem_len])
-data_y = np.reshape(data_y,[nb_training,2])
-
-np.savetxt("./data/data_x.csv", data_x, delimiter=",")
-np.savetxt("./data/data_y.csv", data_y, delimiter=",")
+            np.savetxt(f_x,data_x,delimiter=",")
+            np.savetxt(f_y,data_y,delimiter=",")
