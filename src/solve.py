00318a88ab2425495296455f9e9a1e74e92d8a49
Octooo <114573137+Octooo@users.noreply.github.com>
2022-10-05 00:22:44 +0900

    add solve.py

--- src/solve.py
+++ src/solve.py
@@ -1,3 +1,26 @@
+from os import pread
+import librosa
+from tensorflow.python.keras.models import load_model
+import numpy as np
+
+voice_len = 10000
+
 def solve(path):
+
+  model = np.empty(0)
+  for i in range(88):
+    model = np.append(model,load_model(f'voice_correct_in{voice_num}_div{div}.h5')) #ここに学習後のファイルをぶち込む
+
   with open(path) as file:
+
+    voice,sr =  librosa.load(path)
+    voice = voice[:voice_len]
+    voice = np.reshape(voice,[1,voice_len,1])
+
+    ans = np.empty(0)
+    for i in range(88):
+      pre_data = np.round(model.predict(voice))
+      if pre_data[0] == 1:
+        ans = np.append(ans,i)
+
     return []
