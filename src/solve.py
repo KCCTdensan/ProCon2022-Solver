390cbce68d8c41570ea378201e3963ce69e8e735
Octooo <114573137+Octooo@users.noreply.github.com>
2022-10-08 23:18:43 +0900

    fix

--- src/solve.py
+++ src/solve.py
@@ -1,26 +1,32 @@
-from os import pread
-import librosa
-from tensorflow.python.keras.models import load_model
-import numpy as np
+#!/usr/bin/env python3
 
-voice_len = 10000
+from sklearn import preprocessing
+import numpy as np
+import random
+import librosa
+import os
+
+voice_len = 3000
 
 def solve(path):
 
-  model = np.empty(0)
+  model = []
   for i in range(88):
-    model = np.append(model,load_model(f'voice_correct_in{voice_num}_div{div}.h5')) #main.pyが始まった瞬間にこれを読み込みたい
+    model.append(load_model(f'voice_correct_in{voice_num}.h5')) #main.pyが始まった瞬間にこれを読み込みたい
 
   with open(path) as file:
 
     voice,sr =  librosa.load(path)
+    voice = voice[:10000]
+    voice = np.abs(np.fft.fft(voice))
+    voice = preprocessing.scale(voice)
     voice = voice[:voice_len]
     voice = np.reshape(voice,[1,voice_len,1])
 
     ans = np.empty(0)
     for i in range(88):
 
-      pre_data = np.round(model.predict(voice))
+      pre_data = np.round(model[i].predict(voice))
       
       str = '{:0=2}'.format((i + 1) % 44)
       if pre_data[0] == 1 and  (str in ans) == False:
