10319b6ff045906f30b88414a92860532341e887
Octooo <k.nishigamine@gmail.com>
2022-10-10 10:59:20 +0900

    del data

--- src-training/training.py
+++ src-training/training.py
@@ -15,10 +15,10 @@ print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
 tf.debugging.set_log_device_placement(True)
 strategy = tf.distribute.MirroredStrategy(logical_gpus)
 
-from keras.models import Sequential
+from keras.models import Sequential,Model
 from keras.layers import *
 from keras.callbacks import EarlyStopping
-from keras.optimizers import Adam
+from keras.optimizers import Adam,RMSprop
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import keras.backend as K
@@ -28,26 +28,22 @@ import gc
 
 os.environ['TF_ENABLE_GPU_GARBAGE_COLLECTION']='false'
 
+np.set_printoptions(edgeitems=10)
+
 max_voice_len = 95000
 data_x = np.empty(0)
 data_y = np.empty(0)
-nb_training = 160000
-problem_len = 3000
+nb_training = 100000
+problem_len = int(8192 / 2)
 nb_voice = 88
 
-#create_problem.create(int(nb_training / 500))
-
-file_pathx = "./data/data_x.csv"
-file_pathy = "./data/data_y.csv"
-
-data_x = np.loadtxt(file_pathx, delimiter=',') 
-data_y = np.loadtxt(file_pathy, delimiter=',') 
-
-data_x = np.reshape(data_x,[nb_training,problem_len,1])
-data_y = np.reshape(data_y,[nb_training,nb_voice])
-
 for i in range(88):
-    print(i)
+    print(f'\nvoice_num = {i}')
+
+    data_x,data_y = create_problem.create(nb_training,i)
+
+    data_x = np.reshape(data_x,[nb_training,problem_len,1])
+    data_y = np.reshape(data_y,[nb_training,nb_voice])
 
     voice_num = i
 
@@ -59,8 +55,9 @@ for i in range(88):
             ans = np.append(ans,[0,1])
     
     ans = np.reshape(ans,[nb_training,2])
+    print(ans)
 
-    x_train, x_valid, y_train, y_valid = train_test_split(data_x, ans, test_size=0.1, shuffle= True)
+    x_train, x_valid, y_train, y_valid = train_test_split(data_x, ans, test_size=0.25, shuffle= True)
 
     Input_shape = x_train.shape[1:]
 
@@ -68,17 +65,18 @@ for i in range(88):
         with strategy.scope():
 
             model = Sequential()
-            model.add(Conv1D(128, 4, activation='relu',input_shape=Input_shape))
-            model.add(MaxPool1D(pool_size=2, padding='same'))
-            model.add(Conv1D(64, 4, activation='relu'))
-            model.add(MaxPool1D(pool_size=2, padding='same'))
-            model.add(Conv1D(32, 4, activation='relu'))
-            model.add(MaxPool1D(pool_size=2, padding='same'))
-            model.add(LSTM(64, return_sequences=True))
+            model.add(Conv1D(256, 2, activation='relu',input_shape=Input_shape))
+            model.add(AveragePooling1D(pool_size=2, padding='same'))
+            model.add(Conv1D(128, 2, activation='relu'))
+            model.add(AveragePooling1D(pool_size=2, padding='same'))
+            model.add(Conv1D(64, 2, activation='relu'))
+            model.add(AveragePooling1D(pool_size=2, padding='same'))
+            model.add(Dropout(0.5))
+            model.add(LSTM(32, return_sequences=True))
             model.add(Flatten())
-            model.add(Dense(2,activation='softmax'))
-            model.compile(loss="binary_crossentropy", optimizer=Adam(lr=1e-3),metrics=['accuracy'])
-
+            model.add(Dropout(0.25))
+            model.add(Dense(2,activation='sigmoid'))
+            model.compile(loss="binary_crossentropy", optimizer=RMSprop(lr=1e-3),metrics=['accuracy'])
 
             return model
 
@@ -88,7 +86,7 @@ for i in range(88):
 
     early_stopping =  EarlyStopping(monitor='val_loss',min_delta=0.0,patience=4)
 
-    history = model.fit(x_train, y_train, batch_size=800, epochs=100,verbose=1,validation_data=(x_valid, y_valid),callbacks=[early_stopping])
+    history = model.fit(x_train, y_train, batch_size=256, epochs=100,verbose=1,validation_data=(x_valid, y_valid),callbacks=[early_stopping])
 
     plt.plot(history.epoch, history.history["accuracy"], label="Train accracy")
     plt.plot(history.epoch, history.history["val_accuracy"], label="Validation accracy")
