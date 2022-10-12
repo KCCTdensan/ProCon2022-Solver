8f13ecabbbfba052f6b30ec79ab14d0d77c7498b
Octooo <114573137+Octooo@users.noreply.github.com>
2022-10-12 22:59:20 +0900

    add answer

--- src/solve.py
+++ src/solve.py
@@ -15,18 +15,46 @@ def initModel():
   print("model loaded")
 
 def solve(f):
-  voice, sr = librosa.load(f)
-  voice = voice[:10000]
-  voice = np.abs(np.fft.fft(voice))
-  voice = preprocessing.scale(voice)
-  voice = voice[:voice_len]
-  voice = np.reshape(voice, [1, voice_len, 1])
+  voice, sr = librosa.load(f,sr=48000)
+  voice_len = len(voice)
+  ans = np.zeros(model_n)
+
+  start = 0
+  for i in range(10):
+
+    x = voice[start : start + 20000]
+    x = np.abs(np.fft.fft(x))
+    x = preprocessing.scale(x)
+    start += 100
+
+    for p in range(model_n):
+      pre_data = np.round(model[i].predict(voice))
+      if pre_data[0] == 1:
+        ans[p] += 1
+
+  end = voice_len - (voice_len % 100)
+  for i in range(10):
+
+    x = voice[end - 20000 : end]
+    x = np.abs(np.fft.fft(x))
+    x = preprocessing.scale(x)
+    end -= 100
+
+    for p in range(model_n):
+      pre_data = np.round(model[i].predict(voice))
+      if pre_data[0] == 1:
+        ans[p] += 1
 
   ans = np.empty(0)
   for i in range(model_n):
-    pre_data = np.round(model[i].predict(voice))
-    str = '{:0=2}'.format((i + 1) % 44)
-    if pre_data[0] == 1 and not str in ans:
-      ans = np.append(ans,str)
+    if i < 44 and ans[i] >= 14:
+      str = '{:0=2}'.format((i + 1) % 44)
+      if str not in ans:
+        ans = np.append(ans,str)
 
-  return []
+    elif i >= 44 and ans[i] >= 14:
+      str = '{:0=2}'.format((i + 1) % 44)
+      if str not in ans:
+        ans = np.append(ans,str)
+
+  return ans
