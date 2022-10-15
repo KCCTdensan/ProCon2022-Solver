9b9e9c86317b726c86f480090d0a044618542448
isirmt <r119306@g.kobe-kosen.ac.jp>
2022-10-15 15:27:56 +0900

    fix

--- src/api.py
+++ src/api.py
@@ -65,7 +65,7 @@ async def get_chunk(name):
   raise
 
 async def get_wav(chunk_n):
-  res = await post_json(f"/problem/chunks?n={1 if chunk_n == 0 else chunk_n}")
+  res = await post_json(f"/problem/chunks?n={chunk_n+1}")
   if res.status_code != httpx.codes.OK:
     raise
   name = res.json()["chunks"][chunk_n]
